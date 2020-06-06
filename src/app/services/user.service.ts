b5fad768b5b95a4b2437ff578803e74cf77bc585
Jose Carreres <jose.carreres.81@gmail.com>
2020-06-06 03:58:02 +0200

    Funcionalidad registro de usuario implementada

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+import { global } from './global';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  public url: string;
+  public identity;
+  public token;
+
+
+  constructor( public _http: HttpClient ) {
+    // Almacena la url global
+    this.url = global.url;
+  }
+
+  test(){
+    return "Hola mundo desde un servicio!";
+  }
+
+  // Método para el registro, le pasamos el objeto usuario, nos devolvera un observable
+  register(user): Observable<any>{
+    // Pasa el objeto user a json
+    let json = JSON.stringify(user);
+    // Parametros que se van a enviar a la API
+    let params = 'json='+json;
+
+    // Indica tipo de petición que se va a hacer
+    let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+
+    // Se hace la petición al backend, que será de tipo post
+    // Se le pasa la url, los datos que queremos enviar y en un json se envia la cabecera
+    return this._http.post(this.url+'user', params, {headers: headers});
+  }
 }
