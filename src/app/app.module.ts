ac44173d222f32a35ac143221410d918ac9a8177
Jose Carreres <jose.carreres.81@gmail.com>
2020-06-07 14:08:19 +0200

    Crear archivo guard y proteger las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,10 @@ import { PostNewComponent } from './components/post-new/post-new.component';
 import { PostDetailComponent } from './components/post-detail/post-detail.component';
 import { PostEditComponent } from './components/post-edit/post-edit.component';
 
+import { UserService } from './services/user.service';
+import { IdentityGuard } from './services/identity.guard';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -38,7 +42,11 @@ import { PostEditComponent } from './components/post-edit/post-edit.component';
     FroalaViewModule.forRoot(),
     AngularFileUploaderModule
   ],
-  providers: [],
+  providers: [
+    IdentityGuard,
+    UserService
+  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/identity.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { UserService } from './user.service';
+
+
+@Injectable()
+export class IdentityGuard implements CanActivate{
+  constructor( private _router: Router,
+               private _userService: UserService ){}
+
+  canActivate(){
+    // Comprueba que el usuario este identificado
+    let identity = this._userService.getIdentity();
+
+    if(identity){
+      return true;
+    }else{
+      this._router.navigate(['/error']);
+      return false;
+    }
+  }
+}
